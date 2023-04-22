10b1a55aca727bfe3c6f35fd339ddb5d2c48981f
348670050 <haloday873@gmail.com>
2023-04-22 00:15:37 -0400

    Updates

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,8 @@
 export function Hero() {
   return /*#__PURE__*/React.createElement("div", {
-    className: "w-screen h-screen flex justify-center items-center"
-  }, "Trudeau Physics!");
+    id: "home",
+    className: "snap-center z-[1] w-screen h-screen flex justify-center items-center "
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "font-extrabold text-transparent text-3xl sm:text-5xl lg:text-9xl bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 duration-700"
+  }, "TRUDEAU PHYSICS"));
 }
